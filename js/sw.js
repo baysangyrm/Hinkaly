44fbddc59b7abc8868837e3dc646a640384a0ae9
John Snow <60313748+baysangyrm@users.noreply.github.com>
2022-12-13 02:53:57 +0600

    big update

---
+++ js/sw.js
@@ -0,0 +1,2 @@
+'use strict';
+importScripts('sw-toolbox.js'); toolbox.precache(["../index.html","../css/*","../fonts/*","../css/pages/","../policy.txt"]); toolbox.router.get('/img/*', toolbox.cacheFirst); toolbox.router.get('/*', toolbox.networkFirst, { networkTimeoutSeconds: 5});
